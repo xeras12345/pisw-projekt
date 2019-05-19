b74e641e7d6e5207b654d8e127027d1de45d0dbb
Kornel <kornel.ro@gmail.com>
2019-05-19 15:00:04 +0200

    header v1

---
+++ home.php
@@ -0,0 +1,64 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>Restaurant</title>
+  <meta name="description" content="Restaurant home page">
+  <meta name="author" content="">
+
+  <link rel="stylesheet" href="styles.css" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=swap&subset=latin-ext" rel="stylesheet">
+
+</head>
+
+<body>
+
+<header>
+
+	<div class="section group">
+
+		<div class="col span_9_of_12">
+		<img src="img/talerz1.png" class="responsiveImageHeader">
+		<img src="img/talerz2.png" class="responsiveImageHeader">
+		<img src="img/talerz3.png" class="responsiveImageHeader">
+		</div>
+		<div class="col span_3_of_12">
+		<img src="img/logo.png" class="responsiveImageLogo">
+		</div>
+
+	</div>
+
+</header>
+
+<nav>
+
+	<div class="section group">
+		<ul>
+			<div class="col span_1_of_12">
+			</div>
+			<div class="col span_2_of_12">
+				<li>STRONA GŁÓWNA</li>
+			</div>
+			<div class="col span_2_of_12">
+				<li>MENU</li>
+			</div>
+			<div class="col span_2_of_12">
+				<li>ZAMÓW ONLINE</li>
+			</div>
+			<div class="col span_2_of_12">
+				<li>REZERWACJE</li>
+			</div>
+			<div class="col span_2_of_12">
+				<li>O NAS</li>
+			</div>
+			<div class="col span_1_of_12">
+			</div>
+		</ul>
+	</div>
+
+</nav>
+
+</body>
+</html>
