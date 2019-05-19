42203b7b429e0c7dc4692f6439721068031fae97
Kornel <kornel.ro@gmail.com>
2019-05-19 20:15:39 +0200

    strona glowna

--- home.php
+++ home.php
@@ -17,48 +17,193 @@
 
 <header>
 
+	<nav>
+		<div class="section group">
+			<div class="col span_2_of_12">
+				<img src="img/social (3).png" class="responsiveImageSocial">
+				<img src="img/social (1).png" class="responsiveImageSocial">
+				<img src="img/social (2).png" class="responsiveImageSocial">
+			</div>
+			<div class="col span_5_of_12">
+			</div>
+			<div class="col span_5_of_12">
+				<ul>
+					<li>STRONA GŁÓWNA</li>
+					<li>ZAMÓW ONLINE</li>
+					<li>REZERWACJE</li>
+					<li>O NAS</li>
+				</ul>
+			</div>
+		</div>
+
+	</nav>
+
 	<div class="section group">
 
-		<div class="col span_9_of_12">
-		<img src="img/talerz1.png" class="responsiveImageHeader">
-		<img src="img/talerz2.png" class="responsiveImageHeader">
-		<img src="img/talerz3.png" class="responsiveImageHeader">
+		<div class="col span_4_of_12">
 		</div>
-		<div class="col span_3_of_12">
-		<img src="img/logo.png" class="responsiveImageLogo">
+		<div class="col span_4_of_12">
+		<img src="img/logo (2).png" class="responsiveImageHeader">
 		</div>
+		<div class="col span_4_of_12">
 
 	</div>
 
 </header>
 
-<nav>
+<section>
 
 	<div class="section group">
-		<ul>
-			<div class="col span_1_of_12">
-			</div>
-			<div class="col span_2_of_12">
-				<li>STRONA GŁÓWNA</li>
-			</div>
-			<div class="col span_2_of_12">
-				<li>MENU</li>
-			</div>
-			<div class="col span_2_of_12">
-				<li>ZAMÓW ONLINE</li>
-			</div>
-			<div class="col span_2_of_12">
-				<li>REZERWACJE</li>
-			</div>
-			<div class="col span_2_of_12">
-				<li>O NAS</li>
-			</div>
-			<div class="col span_1_of_12">
-			</div>
-		</ul>
+
+		<div class="col span_12_of_12">
+			<p class="sectionTitle">NASZ SMAK</p>
+		</div>
+
 	</div>
 
-</nav>
+	<div class="section group">
+
+		<div class="col span_1_of_12">
+		</div>
+		<div class="col span_3_of_12">
+			<img src="img/salad.png" class="responsiveImageIcon">
+			<p class="sectionTextBold">ŚWIEŻE SKŁADNIKI</p>
+			<p class="sectionIconText">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique nec ante non rutrum. Etiam mattis sed diam at scelerisque.
+			</p>
+			</div>
+		<div class="col span_4_of_12">
+			<img src="img/menu.png" class="responsiveImageIcon">
+			<p class="sectionTextBold">DOMOWE RECEPTURY</p>
+			<p class="sectionIconText">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique nec ante non rutrum. Etiam mattis sed diam at scelerisque.
+			</p>
+		</div>
+		<div class="col span_3_of_12">
+			<img src="img/chef.png" class="responsiveImageIcon">
+			<p class="sectionTextBold">PROFESJONALIZM</p>
+			<p class="sectionIconText">
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tristique nec ante non rutrum. Etiam mattis sed diam at scelerisque.
+			</p>
+		</div>
+		<div class="col span_1_of_12">
+		</div>
+
+	</div>
+
+</section>
+
+<section>
+
+	<div class="section group">
+
+		<div class="col span_12_of_12">
+			<p class="sectionTitle">NASZE SPECJAŁY</p>
+		</div>
+
+	</div>
+
+	<div class="section group">
+
+		<div class="col span_2_of_12">
+			<img src="img/talerz1.png" class="responsiveImageDish">
+		</div>
+		<div class="col span_4_of_12">
+			<p class="sectionTextBold textMenu">TOST Z AWOKADO NA CIEPŁO</p>
+			<p class="textMenu">chrupiący chleb, awokado, jajko w koszulce, świeża bazylia</p>
+		</div>
+
+		<div class="col span_2_of_12">
+			<img src="img/talerz3.png" class="responsiveImageDish">
+		</div>
+		<div class="col span_4_of_12">
+			<p class="sectionTextBold textMenu">TOST Z AWOKADO NA CIEPŁO</p>
+			<p class="textMenu">chrupiący chleb, awokado, jajko w koszulce, świeża bazylia</p>
+		</div>
+
+
+	</div>
+
+	<div class="section group">
+
+		<div class="col span_2_of_12">
+			<img src="img/talerz4.png" class="responsiveImageDish">
+		</div>
+		<div class="col span_4_of_12">
+			<p class="sectionTextBold textMenu">TOST Z AWOKADO NA CIEPŁO</p>
+			<p class="textMenu">chrupiący chleb, awokado, jajko w koszulce, świeża bazylia</p>
+		</div>
+
+		<div class="col span_2_of_12">
+			<img src="img/talerz5.png" class="responsiveImageDish">
+		</div>
+		<div class="col span_4_of_12">
+			<p class="sectionTextBold textMenu">TOST Z AWOKADO NA CIEPŁO</p>
+			<p class="textMenu">chrupiący chleb, awokado, jajko w koszulce, świeża bazylia</p>
+		</div>
+
+
+	</div>
+
+	<div class="section group">
+
+		<div class="col span_2_of_12">
+			<img src="img/talerz7.png" class="responsiveImageDish">
+		</div>
+		<div class="col span_4_of_12">
+			<p class="sectionTextBold textMenu">TOST Z AWOKADO NA CIEPŁO</p>
+			<p class="textMenu">chrupiący chleb, awokado, jajko w koszulce, świeża bazylia</p>
+		</div>
+
+		<div class="col span_2_of_12">
+			<img src="img/talerz9.png" class="responsiveImageDish">
+		</div>
+		<div class="col span_4_of_12">
+			<p class="sectionTextBold textMenu">TOST Z AWOKADO NA CIEPŁO</p>
+			<p class="textMenu">chrupiący chleb, awokado, jajko w koszulce, świeża bazylia</p>
+		</div>
+
+
+	</div>
+
+</section>
+
+<section id="about">
+
+	<div class="section group">
+
+		<div class="col span_12_of_12">
+			<p class="sectionTitle">NASZE MIEJSCE</p>
+		</div>
+
+	</div>
+
+	<div class="section group">
+
+		<div class="col span_2_of_12">
+		</div>
+		<div class="col span_2_of_12">
+			<p class="sectionTextBold">TELEFON</p>
+			<p class="sectionTextBold">ADRES</p>
+		</div>
+		<div class="col span_2_of_12">
+			<p class="sectionText">500-500-500</p>
+			<p class="sectionText">ul. Kuźnicza 15</p>
+			<p class="sectionText">76-628 Wrocław</p>
+		</div>
+		<div class="col span_4_of_12">
+			<img src="img/mapa.png" class="responsiveImageMap">
+		</div>
+		<div class="col span_2_of_12">
+		</div>
+
+	</div>
+
+</section>
+
+<footer>
+	stoooopka
+</footer>
 
 </body>
 </html>
