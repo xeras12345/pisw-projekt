151f18a9b9857568f33eb2f4494b984eea8387e6
Siwy <mr.gawlowski@gmail.com>
2019-06-10 17:32:43 +0200

    Aktualizacja koszyk

--- koszyk.php
+++ koszyk.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="Page-Enter" content="revealTrans(Duration=2.0,Transition=2)">
-  <title>Restaurant</title>
+  <title>Koszyk</title>
   <meta name="description" content="Restaurant home page">
   <meta name="author" content="">
   <link rel="stylesheet" href="styles.css" type="text/css">
@@ -19,15 +19,57 @@
  
 <?php include("header.php"); ?>
  
- 
+
  
  
 <div id="kontener">
 
     <div id="kontener_koszyk">
         <h1>Koszyk:</h1>
+        
+
+        <h2 style="text-align:center"> Twoje produkty</h2>
+        <table>
+                    <tr>
+                        <th width="10%">Nazwa produktu</th>
+                        <th width="10%">Ilość</th>
+                        <th width="10%">Cena za produkt</th>
+                        <th width="10%">Cena za produkty</th>
+                    </tr>
+        <?php           
+            $total = 0;
+            if(!empty($_SESSION["cart"])){
+                
+                foreach ($_SESSION["cart"] as $key => $value) {
+                    ?>
+                    <tr>
+                        <td><?php echo $value["item_name"]; ?></td>
+                        <td><?php echo $value["item_quantity"]; ?></td>
+                        <td><?php echo $value["product_price"]; ?> zł</td>
+                        <td><?php echo number_format($value["item_quantity"] * $value["product_price"], 2); ?> zł</td>
+                    </tr>
+                    <?php
+                    $total = $total + ($value["item_quantity"] * $value["product_price"]);
+                }
+                    ?>
+                    
+                    
+                    
+                        
+                    
+                    <?php
+                }
+            ?>
+        </table>
+        <h3 style="text-align:center;">Suma:<?php echo number_format($total, 2); ?> zł </h3></br>
+        
+        
+
+
     </div>
 
+
+    <form action="#">
     <div id="dane">
 
         <h1>Twoje Dane:</h1>
@@ -36,19 +78,24 @@
             <h3>Dane Osobowe:</h3>
             <div class="zestaw">
                 <div class="ikonka"><i class="fas fa-user"></i></div>
-                <div class="input_koszyk"><input type="text" placeholder="Wpisz Imię i Nazwisko"/></div>
+                <div class="input_koszyk"><input type="text" placeholder="Wpisz Imię i Nazwisko" required/></div>
             </div>
             <div class="zestaw">
                 <div class="ikonka"><i class="fas fa-envelope"></i></div>
-                <div class="input_koszyk"><input type="text" placeholder="Wpisz email"/></div>
+                <div class="input_koszyk"><input type="text" placeholder="Wpisz email" required/></div>
             </div>
+            <div class="zestaw">
+                <div class="ikonka"><i class="fas fa-phone-alt"></i></div>
+                <div class="input_koszyk"><input type="text" placeholder="Numer telefonu" required/></div>
+            </div>
+
             <div class="zestaw">
                 <fieldset>
 
                     <legend> Rodzaj dostawy </legend>
                     
-                    <div><label><input type="radio" value="1" name="dostawa" checked> Odbiór osobisty</label></div>
-                    <div><label><input type="radio" value="2" name="dostawa" > Do domu</label></div>
+                    <div><label><input type="radio" value="1" name="dostawa" checked required> Odbiór osobisty</label></div>
+                    <div><label><input type="radio" value="2" name="dostawa" required> Do domu</label></div>
                 
             </div>
             <div class="zestaw">
@@ -69,30 +116,39 @@
             <h3>Dane Adresowe:</h3>
             <div class="zestaw">
                 <div class="ikonka"><i class="fas fa-address-card"></i></div>
-                <div class="input_koszyk"><input type="text" placeholder="Wpisz adres zameldowania"/></div>
+                <div class="input_koszyk"><input type="text" placeholder="Wpisz adres zameldowania" required/></div>
             </div>
             <div class="zestaw">
                 <div class="ikonka"><i class="fas fa-city"></i></div>
-                <div class="input_koszyk"><input type="text" placeholder="Wpisz miasto"/></div>
+                <div class="input_koszyk"><input type="text" placeholder="Wpisz miasto" required/></div>
             </div>
             
             <div class="kod">
                 <p>Kod pocztowy:</p>
-            
-                <div class="input_kod"><input type="text" placeholder="np. 58-100"/></div>
+                <div class="input_kod"><input type="text" placeholder="np. 58-100" required /></div>
             </div>
+            
+
+    
+        </div>
+        <div style="clear:both;"></div>
+
+        <div class="uwagi">
+            <label>Uwagi do zamówienia</label></br>
+            <textarea name="komentarz" id="komentarz" rows="4" cols="80" placeholder="Jeśli masz jakieś uwagi, opisz je tutaj..."></textarea>
+        </div>
         
 
-       
-
-
-        </div>
 
     </div>
+    
+    
 
     <div style="clear:both;"></div>
 
-    <div id="przycisk_zamow">Zamów</div>
+    <input type="submit" value="Zamów" id="przycisk_zamow"/>
+    </form>
+    
 
     
 
