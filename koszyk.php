a05c234db3c06aa1800a6d82560d428241e8baad
xeras12345 <mr.gawlowski@gmail.com>
2019-06-15 15:30:44 +0200

    Poprawiłem wygląd koszyka i zrobiłem z niego responsywnego świra

--- koszyk.php
+++ koszyk.php
@@ -7,7 +7,7 @@
   <title>Koszyk</title>
   <meta name="description" content="Restaurant home page">
   <meta name="author" content="">
-  <link rel="stylesheet" href="styles.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="styles.css?version=51">
   <script src="script.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Lato:400,700,900&display=swap&subset=latin-ext" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.9.0/css/all.css">
@@ -21,226 +21,242 @@
 <?php include("header.php"); ?>
 
 
+<div class="section"> 
  
- 
-<div id="kontener">
-
-    <div id="kontener_koszyk">
-        <h1>Koszyk:</h1>
+    <section id="kontejner_koszyk">
         
-        
-
-        <h2 style="text-align:center"> Twoje produkty</h2>
-        <table>
-                    <tr>
-                        <th width="10%">Nazwa produktu</th>
-                        <th width="10%">Ilość</th>
-                        <th width="10%">Cena za produkt</th>
-                        <th width="10%">Cena za produkty</th>
-                    </tr>
-                
-        <?php           
-            $total = 0;
-        
-            if(!empty($_SESSION["cart"])){
-                $outall = '';
-                foreach ($_SESSION["cart"] as $key => $value) {
-                    
-                    ?>
-                    
-                    <?php $produkty = $value["item_name"];?>
-
-                    <tr>
-                        <td id="nazwa"><?php echo $value["item_name"]; 
-                         $outall = ''.$outall.$value["item_name"].'/';?></td>
-                        <td><?php echo $value["item_quantity"]; ?></td>
-                        <td><?php echo $value["product_price"]; ?> zł</td>
-                        <td><?php echo number_format($value["item_quantity"] * $value["product_price"], 2); ?> zł</td>
-                        <a href="zamowonline.php?action=delete&id=<?php echo $value["product_id"]; ?>"><span id="click"></span></a>
-                        
-                        
-                           
-                    </tr>
-                    <?php
-                    $total = $total + ($value["item_quantity"] * $value["product_price"]);
-                    
-                    
-                }
-                
-                    ?>
-                    
-           
-                
-                
-                    
-                    
-                        
-                    
-                    <?php
-                    $kwota = $total;
-                    
-                    
-                }
-            ?>
-        </table>
-        <h3 style="text-align:center;">Suma:<?php echo number_format($total, 2); ?> zł </h3></br>
+        <div class="section group">
             
-        
+                <p class="sectionTitle">Koszyk:<p>
+                <p class="sectionTitle"> Twoje produkty</p>
+                <div class="col span_1_of_12"></div>
+                <div class="col span_3_of_12"><p class="sectionTextBold textMenu">Nazwa produktu</p></div>
+                <div class="col span_2_of_12"><p class="sectionTextBold textMenu">Ilość</p></div>
+                <div class="col span_3_of_12"><p class="sectionTextBold textMenu">Cena za produkt</p></div>
+                <div class="col span_3_of_12"><p class="sectionTextBold textMenu">Cena za produkty</p></div>
 
-    
-
-    </div>
-
-<?php 
-    if(isset($_SESSION["loggedin"])){
-
-        echo'<form id="myForm" action="koszyk.php" method="post">
-        <div id="dane">
-
-            <h1>Twoje Dane:</h1>
+        </div>
             
-            <div id="dane_osobowe">
-                <h3>Dane Osobowe:</h3>
-                <div class="zestaw">
-                    <div class="ikonka"><i class="fas fa-user"></i></div>
-                    <div class="input_koszyk"><input type="text" placeholder="Wpisz Imię i Nazwisko" /></div>
-                </div>
-                <div class="zestaw">
-                    <div class="ikonka"><i class="fas fa-envelope"></i></div>
-                    <div class="input_koszyk"><input type="email" name="email" value='.$_SESSION["email1"].' readonly/></div>
-                </div>
-                <div class="zestaw">
-                    <div class="ikonka"><i class="fas fa-phone-alt"></i></div>
-                    <div class="input_koszyk"><input type="text" name="numer" placeholder="Numer telefonu" /></div>
-                </div>
-
-                <div class="zestaw">
-                    <fieldset>
-
-                        <legend> Rodzaj dostawy </legend>
-                        
-                        <div><label><input type="radio" value="1" name="dostawa" checked required> Odbiór osobisty</label></div>
-                        <div><label><input type="radio" value="2" name="dostawa" required> Do domu</label></div>
-                    
-                </div>
-                <div class="zestaw">
-                    <fieldset>
-
-                        <legend> Metoda płatności </legend>
-                        
-                        <div><label><input type="radio" value="1" name="platnosc" checked disabled> Przy odbiorze</label></div>
-                    
-                    
-                </div>
-
-
-                
-            </div>
-
-            <div id="dane_adresowe">
-                <h3>Dane Adresowe:</h3>
-                <div class="zestaw">
-                    <div class="ikonka"><i class="fas fa-address-card"></i></div>
-                    <div class="input_koszyk"><input type="text" name="adres" placeholder="Wpisz adres dostawy" /></div>
-                </div>
-                <div class="zestaw">
-                    <div class="ikonka"><i class="fas fa-city"></i></div>
-                    <div class="input_koszyk"><input type="text" name="miasto" placeholder="Wpisz miasto" /></div>
-                </div>
-                
-                <div class="kod">
-                    <p>Kod pocztowy:</p>
-                    <div class="input_kod"><input type="text" placeholder="np. 58-100" /></div>
-                </div>
-                
-
-        
-            </div>
-            <div style="clear:both;"></div>
-
-            <div class="uwagi">
-                <label>Uwagi do zamówienia</label></br>
-                <textarea name="uwagi" id="komentarz" rows="4" cols="80" placeholder="Jeśli masz jakieś uwagi, opisz je tutaj..."></textarea>
-            </div>
             
 
+            <div class="section group">        
+            <?php           
+                $total = 0;
+            
+                if(!empty($_SESSION["cart"])){
+                    $outall = '';
+                    foreach ($_SESSION["cart"] as $key => $value) {
+                        
+                        ?>
+                        
+                        <?php $produkty = $value["item_name"];?>
+
+                        <div class="col span_1_of_12"></div>
+                        <div class="col span_2_of_12"><p class="textMenu"><?php echo $value["item_name"];
+                            $outall = ''.$outall.$value["item_name"].'/';?></p></div>
+                        <div class="col span_1_of_12"></div>     
+                        <div class="col span_2_of_12"><p class="textMenu"><?php echo $value["item_quantity"]; ?></p></div>
+        
+                        <div class="col span_2_of_12"><p class="textMenu"><?php echo $value["product_price"]; ?> zł</p></div>
+                        <div class="col span_1_of_12"></div>
+                        <div class="col span_2_of_12"><p class="textMenu"><?php echo number_format($value["item_quantity"] * $value["product_price"], 2); ?> zł</p></div>
+                        <div class="col span_1_of_12"></div>
+
+                            <a href="zamowonline.php?action=delete&id=<?php echo $value["product_id"]; ?>"><span id="click"></span></a>
+                        
+                        <?php
+                        $total = $total + ($value["item_quantity"] * $value["product_price"]);
+                        
+                        
+                    }
+                    
+                        ?>
+                        
+            
+                    
+                    
+                        
+                        
+                            
+                        
+                        <?php
+                        $kwota = $total;
+                        
+                        
+                    }
+                ?>
+
+            
+            </div>
+            <div class="section group">
+            <p class="sectionTitle">Suma:<?php echo number_format($total, 2); ?> zł </p>
+            </div>
 
         </div>
         
-        
+    </section>
+    <section>
+    <?php 
+        if(isset($_SESSION["loggedin"])){
 
-            <div style="clear:both;"></div>'
-            ?>
-            
-            <div id="przycisk_zamow" onclick="document.getElementById('okno_potwierdzajace').style.display='block'">Złóż zamówienie</div>
-            <div id="okno_potwierdzajace" class="okno">
-                <div class="okno-content animate">
-                <h4 style="text-align:center">Czy na pewno chcesz złożyć zamówienie?</h4></br>
-                    <div>
-                        <input id="submitform" name="submitform" type="submit"  class="btn" value="Tak" onclick="usun();">
-                        <input type="button"  class="btn" value="Nie" onclick="document.getElementById('okno_potwierdzajace').style.display='none'">
+            echo'<form id="myForm" action="koszyk.php" method="post">
+            <div class="section group">
+
+                <p class="sectionTitle">Twoje Dane:<p>
+                
+                <div id="dane_osobowe" class="col span_6_of_12">
+                    <p class="sectionTitle">Dane Osobowe:<p>
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <div class="ikonka"><i class="fas fa-user"></i></div>
+                        <div class="input_koszyk"><input type="text" placeholder="Wpisz Imię i Nazwisko" /></div>
+                        </div>
                     </div>
-            </div>
-            </form>
-        <?php
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <div class="ikonka"><i class="fas fa-envelope"></i></div>
+                        <div class="input_koszyk"><input type="email" name="email" value='.$_SESSION["email1"].' readonly/></div>
+                        </div>
+                    </div>
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <div class="ikonka"><i class="fas fa-phone-alt"></i></div>
+                        <div class="input_koszyk"><input type="text" name="numer" placeholder="Numer telefonu" /></div>
+                        </div>
+                    </div>
 
-        
-        } 
-        ?>
-    
-  
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <fieldset>
 
-        
-    </div>
+                            <legend> Rodzaj dostawy </legend>
+                            
+                            <div><label><input type="radio" value="1" name="dostawa" checked required> Odbiór osobisty</label></div>
+                            <div><label><input type="radio" value="2" name="dostawa" required> Do domu</label></div>
+                        </div>
+                    </div>
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <fieldset>
 
-    </div>
+                            <legend> Metoda płatności </legend>
+                            
+                            <div><label><input type="radio" value="1" name="platnosc" checked disabled> Przy odbiorze</label></div>
+                        
+                        </div>
+                    </div>
 
-        
 
+                    
+                </div>
 
+                <div id="dane_adresowe" class="col span_6_of_12">
+                    <p class="sectionTitle">Dane Adresowe:<p>
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <div class="ikonka"><i class="fas fa-address-card"></i></div>
+                        <div class="input_koszyk"><input type="text" name="adres" placeholder="Ulica i numer lokalu" /></div>
+                        </div>
+                    </div>
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <div class="ikonka"><i class="fas fa-city"></i></div>
+                        <div class="input_koszyk"><input type="text" name="miasto" placeholder="Wpisz miasto" /></div>
+                        </div>
+                    </div>
+                    
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <div class="ikonka"><i class="fas fa-map-marker-alt"></i></div>
+                        <div class="input_koszyk"><input type="text" name="miasto" placeholder="Kod pocztowy(XX-XXX)" /></div>
+                        </div>
+                    </div>
+                    <div class="section group">
+                        <div class="col span_12_of_12">
+                        <label>Uwagi do zamówienia</label></br>
+                        <textarea name="uwagi" id="komentarz" rows="8" cols="80" placeholder="Jeśli masz jakieś uwagi, opisz je tutaj..."></textarea>
+                        </div>
+                    </div>
 
-
-
-</div>
-
-<?php
-    require_once "connect.php";
-    $date = date('Y-m-d');
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $adres = isset($_POST['adres']) ? $_POST['adres'] : '';
-    $miasto = isset($_POST['miasto']) ? $_POST['miasto'] : '';
-    $numer = isset($_POST['numer']) ? $_POST['numer'] : '';
-    $uwagi = isset($_POST['uwagi']) ? $_POST['uwagi'] : '';
-    if (isset($_POST['submitform']))
-     {   
-
-        $link->query("SET NAMES 'utf8'");
-        if ($link->connect_error) {
-            die("Connection failed: " . $link->connect_error);
-        } 
-
-        $sql = "INSERT INTO zamowienia (email, kwota, produkty, adres, miasto, numer, uwagi, status, datazamowienia)
-        VALUES ('$email', $kwota, '$outall','$adres','$miasto','$numer','$uwagi',0,'$date')";
-
-        if ($link->query($sql) === TRUE) {
             
-        } else {
-            echo "Error: " . $sql . "<br>" . $link->error;
+                </div>
+     
+
+
+            </div>'
+                
+                ?>    
+    </section>
+                <section>
+             
+                <div class="section group">
+                    <div id="przycisk_zamow" onclick="document.getElementById('okno_potwierdzajace').style.display='block'"><p class="sectionTextBold textMenu">Złóż zamówienie</p></div>
+                    <div id="okno_potwierdzajace" class="okno">
+                        <div class="okno-content animate">
+                        <div class="section group">
+                        <div class="col span_12_of_12">
+                        <p class="sectionTextBold textMenu" style="text-align:center;">Czy na pewno chcesz złożyć zamówienie?</p>
+                        <div>
+                            <input id="submitform" name="submitform" type="submit"  class="btn" value="Tak" onclick="usun();">
+                            <input type="button"  class="btn" value="Nie" onclick="document.getElementById('okno_potwierdzajace').style.display='none'">
+                        </div>
+        </div>
+                        </div>
+        </div>
+                    </div>
+                </div>
+                </form>
+            
+
+                <?php
+            
+                } 
+                ?>
+                
+
+
+
+    </div>
+    </section>  
+
+    <?php
+        require_once "connect.php";
+        $date = date('Y-m-d');
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $adres = isset($_POST['adres']) ? $_POST['adres'] : '';
+        $miasto = isset($_POST['miasto']) ? $_POST['miasto'] : '';
+        $numer = isset($_POST['numer']) ? $_POST['numer'] : '';
+        $uwagi = isset($_POST['uwagi']) ? $_POST['uwagi'] : '';
+        if (isset($_POST['submitform']))
+        {   
+
+            $link->query("SET NAMES 'utf8'");
+            if ($link->connect_error) {
+                die("Connection failed: " . $link->connect_error);
+            } 
+
+            $sql = "INSERT INTO zamowienia (email, kwota, produkty, adres, miasto, numer, uwagi, status, datazamowienia)
+            VALUES ('$email', $kwota, '$outall','$adres','$miasto','$numer','$uwagi',0,'$date')";
+
+            if ($link->query($sql) === TRUE) {
+                
+            } else {
+                echo "Error: " . $sql . "<br>" . $link->error;
+            }
+
+
+            $link->close();
+            ?>
+            <script>
+                window.location = "podsumowanie.php";
+            </script>
+
+            <?php
         }
 
 
-        $link->close();
-        ?>
-        <script>
-            window.location = "podsumowanie.php";
-        </script>
-
-        <?php
-     }
-
-
-?>
-  
+    ?>
+</div>
     
 
   <script>
